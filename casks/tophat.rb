ce10ba328faeb237209bdddbf43aef0ef41cb05f
Jared Hendry <105017+jaredh@users.noreply.github.com>
2025-06-03 14:56:12 -0400

    Update Tophat to 2.1.2

--- casks/tophat.rb
+++ casks/tophat.rb
@@ -1,6 +1,6 @@
 cask "tophat" do
-  version "2.1.1"
-  sha256 "aab4421876768dec94262f3c887a3b5d0084e59a3b3ce046e4963f588802e176"
+  version "2.1.2"
+  sha256 "1d0a70142c767e5b6ae7a6e79ed7d0d604c31159e483fceeb7a3fc9cf515a770"
 
   url "https://github.com/Shopify/tophat/releases/download/v#{version}/Tophat-#{version}.zip"
   name "Tophat"
